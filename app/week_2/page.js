39cc5f3e2dca545940d96f0d5f1d0b0311e1318f
ishamjotkaur <ishamjotkaur17@gmail.com>
2025-01-14 10:28:16 -0700

    Week-2 demo

---
+++ app/week_2/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+      <div>
+        <h1> Welcome to Week-2! </h1>
+        <p> <Link href="/">Go to Home </Link></p>
+      </div>
+    );
+  }
