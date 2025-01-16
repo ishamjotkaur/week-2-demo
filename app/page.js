f22722535432070042af3291663d3e2d8f191149
ishamjotkaur <ishamjotkaur17@gmail.com>
2025-01-16 09:55:19 -0700

    Added a Component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import Link from "next/link";
 export default function Home() {
+  let a = 5;
+  let b = 10;
   return (
     <div>
       <h1> My App Week-2 </h1>
       <p> <Link href="/week_2"> Go to Week2 </Link> </p>
+      <p>The addition is: {a + b} </p>
     </div>
   );
 }
--- app/week_2/page.js
+++ app/week_2/page.js
@@ -1,8 +1,12 @@
 import Link from "next/link";
-
+import Title from "./title";
 export default function Page() {
     return (
       <div>
+        <Title />
+        <Title />
+        <Title />
+        <Title />
         <h1> Welcome to Week-2! </h1>
         <p> <Link href="/">Go to Home </Link></p>
       </div>
